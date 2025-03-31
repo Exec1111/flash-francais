d69e17276605884801c3d5e155b304f1e4e3dc8f
Exec1111 <julien.vachey@gmail.com>
2025-03-31 21:30:23 +0200

    dsqdq

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum
 from sqlalchemy.sql import func
-from database import Base
+from backend.database import Base
 import enum
 
 class UserRole(enum.Enum):
--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,1 +1,6 @@
-from routers.auth import router as auth_router
+from backend.routers.auth import router as auth_router
+# Importez d'autres routeurs ici si nécessaire
+# from backend.routers.autre import router as autre_router
+
+# Vous pouvez regrouper les routeurs ou les exposer individuellement
+__all__ = ["auth_router"] # Ajoutez d'autres noms de routeurs exportés
